1a462a123adbf884702d89d2b2e300a12b377bd6
Jim Stagge <jhstagge@gmail.com>
2020-10-13 17:52:24 -0400

    Completed Structural Change

--- worked_examples/render_wordDocuments.R
+++ worked_examples/render_wordDocuments.R
@@ -1,8 +1,12 @@
 library(purrr)
 library(rmarkdown)
-files <- list.files("rmarkdown", pattern = "^[567].*\\.Rmd$", full.names = T)
+files <- list.files("rmarkdown", pattern = "^[1567].*\\.Rmd$", full.names = T)
+#files <- list.files("rmarkdown", pattern = ".*\\.Rmd$", full.names = T)
 #files <- files[c(8)]
-files <- files[c(seq(1,11),13)]
+#files <- files[c(seq(1,11),13)]
+files <- files[seq(2, 15)]
+
+files <- files[c(14,15)]
 walk(files, render, output_format = "word_document", output_dir = "word", envir = new.env())
 
 
@@ -12,3 +16,7 @@ rmarkdown::render_site(input = "rmarkdown", output)
 
 
 rmarkdown::render_site()
+
+
+
+
