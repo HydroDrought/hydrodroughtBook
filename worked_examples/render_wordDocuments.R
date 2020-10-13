01c7fdfa8fbc4520672276d3c686f7b9492a2600
Jim Stagge <jhstagge@gmail.com>
2020-10-13 14:02:24 -0400

    Fixed Worked Example 7.2 and deleted old versions

--- worked_examples/render_wordDocuments.R
+++ worked_examples/render_wordDocuments.R
@@ -1,5 +1,6 @@
 library(purrr)
 library(rmarkdown)
-files <- list.files("rmarkdown", pattern = "^[56].*\\.Rmd$", full.names = T)
-files <- files[c(8)]
+files <- list.files("rmarkdown", pattern = "^[567].*\\.Rmd$", full.names = T)
+#files <- files[c(8)]
+files <- files[c(seq(1,11),13)]
 walk(files, render, output_format = "word_document", output_dir = "word", envir = new.env())
