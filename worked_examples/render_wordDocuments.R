a71b0bbe654720fc6fe2412c7e4c28ed98c51b6b
Jim Stagge <jhstagge@gmail.com>
2021-08-09 11:16:02 -0400

    Reran the bookdown and worked example creation

--- worked_examples/render_wordDocuments.R
+++ worked_examples/render_wordDocuments.R
@@ -7,6 +7,8 @@ files <- list.files("rmarkdown", pattern = "^[1567].*\\.Rmd$", full.names = T)
 files <- files[seq(2, 15)]
 
 files <- files[c(14,15)]
+
+files <- files[seq(14,17)]
 walk(files, render, output_format = "word_document", output_dir = "word", envir = new.env())
 
 
@@ -18,5 +20,9 @@ rmarkdown::render_site(input = "rmarkdown", output)
 rmarkdown::render_site()
 
 
+
+
+
+walk(files, render, output_format = "word_document", output_dir = "word", envir = new.env())
 
 
